d9b8f4f7c5ab95e4bb3742fd4d15209399171b7c
Abdelhafid ALLOU <20010297>
2019-12-15 23:43:34 +0100

    Add k8s deployment

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (request, response) => {
-  response.json({"version": "0.0.1"});
+app.get("/", (request, response) => {
+  response.json({ Hello: "world" });
 });
 
-app.listen(8081, () => console.info('Application running on port 8081'));
+app.listen(8081, () => console.info("Application running on port 8081"));
